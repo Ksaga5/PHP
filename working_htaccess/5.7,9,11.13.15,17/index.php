df25238f5540fce1ed52b0f71a34370082d5be90
Ksaga <52404149+Ksaga5@users.noreply.github.com>
2021-01-12 18:30:19 +0700

    up
    up

---
+++ working_htaccess/5.7,9,11.13.15,17/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>1</title>
+    </head>
+    <body>
+        <?php
+        echo'index HTML';
+       var_dump($_SERVER['REQUEST_URI']);
+        ?>
+    </body>
+</html>
---
+++ working_htaccess/5.7,9,11.13.15,17/index2.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>1</title>
+    </head>
+    <body>
+        <?php
+        echo'index2 NULL';
+        ?>
+    </body>
+</html>
